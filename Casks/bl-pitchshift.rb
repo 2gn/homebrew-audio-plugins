b984b28ad6457237954fdfa5f57e1bcc6b4b5763
2gn <101851090+2gn@users.noreply.github.com>
2022-12-28 16:09:51 +0900

    bl-pitchshift: init at 5.0.5

---
+++ Casks/bl-pitchshift.rb
@@ -0,0 +1,12 @@
+cask "bl-pitchshift" do
+  version "5.0.5"
+  sha256 "fdcead4aac92fd8b147708f612d4405daf2459e486d52f4e844971d7e55e1536"
+
+  url "https://plugins4free.com/get_plug/BL-PitchShift-v#{version}-mac.zip"
+  name "BL-PitchShift"
+  desc "a pitch shifter"
+  homepage "https://plugins4free.com/plugin/3622/"
+
+  app "PitchShift.app"
+
+end
